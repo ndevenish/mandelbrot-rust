eac04213b0847e2967a55cc40a9a65f0c71292ec
Nicholas Devenish <ndevenish@gmail.com>
2022-01-23 11:42:39 +0000

    Alternate approach to tiff without altering base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+
+use tiff::encoder::{colortype, TiffEncoder};
+
 fn mandelbrot(r: f32, i: f32) -> u32 {
     let mut r0: f32 = 0.0;
     let mut i0: f32 = 0.0;
@@ -23,5 +27,44 @@ fn main() {
             let r: f32 = -2.0 + 0.01 * _r as f32;
             ms[_i][_r] = mandelbrot(r, i)
         }
+    }
+    // Convert this to a 1D vector
+    let data: Vec<u32> = ms.iter().flatten().cloned().collect();
+
+    let mut file = File::create(r"test.tiff").unwrap();
+    let mut tiff = TiffEncoder::new(&mut file).unwrap();
+    tiff.write_image::<colortype::Gray32>(400, 400, &data)
+        .unwrap();
+
+    print_block(0, 0, 30, 30, 400, 400, &ms);
+}
+
+/// Print out a block of data as an ascii table
+fn print_block(
+    x: usize,
+    y: usize,
+    view_width: usize,
+    view_height: usize,
+    image_w: usize,
+    image_h: usize,
+    data: &[[u32; 400]],
+) {
+    let left: usize = std::cmp::max(x, 0);
+    let right: usize = std::cmp::min(x + view_width, image_w);
+    let top: usize = std::cmp::max(y, 0);
+    let bottom: usize = std::cmp::min(y + view_height, image_h);
+
+    print!("     ");
+    for x in left..right {
+        print!("{x:4} ")
+    }
+    print!("\n   ┌─{}", "─".repeat(view_width as usize * 5));
+    println!();
+    for x in left..right {
+        print!("{x:2} │ ");
+        for y in top..bottom {
+            print!("{:4} ", data[y][x]);
+        }
+        println!();
     }
 }
