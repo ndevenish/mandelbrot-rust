30bfec2b5b3216e813e326e30f888b0f18af6a54
Nicholas Devenish <ndevenish@gmail.com>
2022-01-23 11:27:08 +0000

    Tweaks and output
    - Add a function to dump a portion of the array to terminal
    - Use a 1D array (it's probably easier to abstract 1d->2d than the other way)
    - use the tiff crate to write out an image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{
+    cmp::{max, min},
+    fs::File,
+};
+
+use tiff::encoder::{colortype, TiffEncoder};
+
 fn mandelbrot(r: f32, i: f32) -> u32 {
     let mut r0: f32 = 0.0;
     let mut i0: f32 = 0.0;
@@ -13,15 +20,51 @@ fn mandelbrot(r: f32, i: f32) -> u32 {
     }
     return c;
 }
-
 fn main() {
     // scan along i = 1.0 from -2 to +2, steps of 0.01
-    let mut ms: [[u32; 400]; 400] = [[0; 400]; 400];
+    let mut ms = [0; 400 * 400];
+
     for _i in 0..400 {
         let i: f32 = -2.0 + 0.01 * _i as f32;
         for _r in 0..400 {
             let r: f32 = -2.0 + 0.01 * _r as f32;
-            ms[_i][_r] = mandelbrot(r, i)
+            ms[_i * 400 + _r] = mandelbrot(r, i)
         }
+    }
+    let mut file = File::create(r"test.tiff").unwrap();
+    let mut tiff = TiffEncoder::new(&mut file).unwrap();
+    tiff.write_image::<colortype::Gray32>(400, 400, &ms)
+        .unwrap();
+
+    print_block(0, 0, 30, 30, 400, 400, &ms);
+}
+
+/// Print out a block of data as an ascii table
+fn print_block(
+    x: u32,
+    y: u32,
+    view_width: u32,
+    view_height: u32,
+    image_w: u32,
+    image_h: u32,
+    data: &[u32],
+) {
+    let left = max(x, 0);
+    let right = min(x + view_width, image_w);
+    let top = max(y, 0);
+    let bottom = min(y + view_height, image_h);
+
+    print!("     ");
+    for x in left..right {
+        print!("{x:4} ")
+    }
+    print!("\n   ┌─{}", "─".repeat(view_width as usize * 5));
+    println!();
+    for x in left..right {
+        print!("{x:2} │ ");
+        for y in top..bottom {
+            print!("{:4} ", data[(y * image_w + x) as usize]);
+        }
+        println!();
     }
 }
